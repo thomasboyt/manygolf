c813b4f213bf70fea7f60e063c587737050a054c
Thomas Boyt <thomas@ledgerx.com>
2016-03-22 16:58:35 -0400

    Lint

--- client/app/store.js
+++ client/app/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import appReducer from './reducers';
