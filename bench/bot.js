9b30cb86dada161f67b068bbf3b27048a10dc58f
Thomas Boyt <me@thomasboyt.com>
2016-05-24 06:40:19 -0400

    Add random reconnects to bench bot

--- bench/bot.js
+++ bench/bot.js
@@ -11,18 +11,28 @@ function randInt(min, max) {
 function addBot() {
   const ws = new WebSocket('ws://localhost:4080');
 
-  ws.on('open', () => {
-    setInterval(() => {
-      ws.send(JSON.stringify({
-        type: 'swing',
-        data: {
-          vec: {
-            x: randInt(-100, 100),
-            y: randInt(-100, 100),
-          }
+  function cb() {
+    if (randInt(1, 10) === 1) {
+      ws.close();
+      addBot();
+      return;
+    }
+
+    ws.send(JSON.stringify({
+      type: 'swing',
+      data: {
+        vec: {
+          x: randInt(-100, 100),
+          y: randInt(-100, 100),
         }
-      }));
-    }, randInt(1000, 2000));
+      }
+    }));
+
+    setTimeout(cb, randInt(1000, 2000));
+  }
+
+  ws.on('open', () => {
+    setTimeout(cb, randInt(1000, 2000));
   });
 }
 
