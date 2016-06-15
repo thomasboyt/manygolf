965fd52e02f4e52eaeb751973ef768570f9b7df5
Thomas Boyt <me@thomasboyt.com>
2016-06-15 02:31:34 -0400

    [client] Add podium screen

--- src/client/render/matchEnd.ts
+++ src/client/render/matchEnd.ts
@@ -5,7 +5,89 @@ import {
 
 import {
   State,
+  MatchEndPlayer,
 } from '../records';
 
+const podiumHeights = new Map<number, number>().set(1, 100).set(2, 75).set(3, 50);
+
+function drawHalfCrown(ctx: CanvasRenderingContext2D) {
+  ctx.beginPath();
+  ctx.moveTo(10, 40);
+  ctx.lineTo(0, 0);
+  ctx.lineTo(20, 30);
+  ctx.lineTo(30, 0);
+  ctx.lineTo(40, 30);
+  ctx.lineTo(40, 40);
+  ctx.lineTo(10, 40);
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawCrown(ctx: CanvasRenderingContext2D, ballX: number, ballY: number) {
+  ctx.save();
+
+  ctx.strokeStyle = 'black';
+  ctx.fillStyle = 'yellow';
+
+  ctx.save();
+  ctx.translate(ballX - 40, ballY - 80)
+  drawHalfCrown(ctx);
+  ctx.restore();
+
+  ctx.save();
+  ctx.translate(ballX + 40, ballY - 80)
+  ctx.scale(-1, 1);
+  drawHalfCrown(ctx);
+  ctx.restore();
+
+  ctx.restore();
+}
+
+function drawPodium(
+  ctx: CanvasRenderingContext2D, player: MatchEndPlayer, position: number, x: number
+) {
+  // can't draw a podium soooo
+  if (!player) {
+    return;
+  }
+
+  const bottomY = HEIGHT - 50;
+
+  const podiumHeight = podiumHeights.get(position);
+
+  ctx.fillStyle = 'gray';
+  ctx.fillRect(x - 50, bottomY - podiumHeight, 100, podiumHeight);
+
+  ctx.beginPath();
+  const ballY = bottomY - podiumHeight - 50;
+  ctx.arc(x, ballY, 50, 0, 2 * Math.PI);
+  ctx.strokeStyle = 'white';
+  ctx.fillStyle = player.color;
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath();
+
+  if (position === 1) {
+    drawCrown(ctx, x, ballY);
+  }
+
+  ctx.textAlign = 'center';
+  ctx.fillStyle = 'black';
+
+  ctx.font = 'normal 16px "Press Start 2P"';
+  ctx.fillText(`${position}`, x, bottomY - podiumHeight + 25);
+
+  ctx.font = 'normal 8px "Press Start 2P"';
+  ctx.fillText(`${player.points} points`, x, bottomY - podiumHeight + 40);
+
+  ctx.fillStyle = 'white';
+  ctx.fillText(`${player.name}`, x, bottomY + 15);
+}
+
 export default function renderMatchEnd(ctx: CanvasRenderingContext2D, state: State) {
+  const players = state.matchRankedPlayers;
+
+  drawPodium(ctx, players.get(0), 1, 300);
+  drawPodium(ctx, players.get(0), 2, 100);
+  drawPodium(ctx, players.get(0), 3, 500);
 }
--- src/testData.ts
+++ src/testData.ts
@@ -1,20 +1,20 @@
 const msgs = [
   {
     time: 0,
-    msg: {"type":"initial","data":{"gameState":0,"self":{"id":2,"color":"#b74bed","name":"Amazing Backspin"},"isObserver":false,"players":[{"id":2,"color":"#b74bed","name":"Amazing Backspin","position":[46,184.5],"velocity":[0,0]}],"level":{"points":[[0,191],[36,187],[56,187],[97,151],[129,177],[150,207],[169,207],[186,207],[234,172],[264,192],[283,228],[310,228],[349,237],[395,209],[427,209],[444,209],[472,203],[508,234],[546,234],[568,239],[586,239],[600,208]],"hole":[577,239],"spawn":[46,187],"color":"red"},"expiresIn":34218,"time":1465512947084}},
+    msg: {"type":"initial","data":{"gameState":0,"self":{"id":3,"color":"#b74bed","name":"Amazing Backspin"},"isObserver":false,"players":[{"id":2,"color":"#b74bed","name":"Amazing Backspin","position":[46,184.5],"velocity":[0,0]}],"level":{"points":[[0,191],[36,187],[56,187],[97,151],[129,177],[150,207],[169,207],[186,207],[234,172],[264,192],[283,228],[310,228],[349,237],[395,209],[427,209],[444,209],[472,203],[508,234],[546,234],[568,239],[586,239],[600,208]],"hole":[577,239],"spawn":[46,187],"color":"red"},"expiresIn":34218,"time":1465512947084}},
   },
   {
     time: 200,
     msg: {
-      "type":"levelOver",
+      "type":"matchOver",
       "data": {
         "expTime": Date.now() + 6000,
-        "roundRankedPlayers": [
-          {"id":2,"color":"#b74bed","name":"Amazing Backspin","strokes":4,"scoreTime":33627,"prevPoints":20,"addedPoints":10, scored: true},
-          {"id":3,"color":"#b74bed","name":"Fred Goodatgolf","strokes":4,"scoreTime":33627,"prevPoints":50,"addedPoints":8, scored: true},
-          {"id":7,"color":"#b74bed","name":"asdfadsfads asdfdasf","strokes":4,"scoreTime":33627,"prevPoints":500,"addedPoints":6, scored: true},
-          {"id":9,"color":"#b74bed","name":"garbage text","strokes":4,"scoreTime":33627,"prevPoints":20,"addedPoints":4, scored: true},
-          {"id":5,"color":"#b74bed","name":"blah foo","strokes":4,"scoreTime":33627,"prevPoints":10,"addedPoints":0, scored: false},
+        "matchRankedPlayers": [
+          {"id":2,"color":"#b74bed","name":"Amazing Backspin","points":100},
+          {"id":3,"color":"#b74bed","name":"Amazing Backspin","points":80},
+          {"id":4,"color":"#b74bed","name":"Amazing Backspin","points":40},
+          {"id":5,"color":"#b74bed","name":"Amazing Backspin","points":30},
+          {"id":6,"color":"#b74bed","name":"Amazing Backspin","points":20},
         ]
       }
     }
