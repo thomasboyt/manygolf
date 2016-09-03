a3eca2cf613c01901145a51b4846a30e3c7cbe56
Thomas Boyt <me@thomasboyt.com>
2016-09-02 23:26:01 -0400

    Fix double rendering player ball due to merge error

--- src/client/render/index.ts
+++ src/client/render/index.ts
@@ -260,14 +260,6 @@ function renderBalls(ctx: CanvasRenderingContext2D, state: State) {
   ctx.fill();
   ctx.closePath();
 
-  ctx.beginPath();
-  ctx.arc(ballPos[0], ballPos[1], 2.5, 0, 2 * Math.PI);
-  ctx.fillStyle = ballColor;
-  ctx.strokeStyle = ballColor; // add stroke so it's the same size as the ghosts
-  ctx.fill();
-  ctx.stroke();
-  ctx.closePath();
-
   renderBall(ctx, ballPos[0], ballPos[1], ballColor, ballColor);
 
   if (state.match.leaderId === state.id) {
