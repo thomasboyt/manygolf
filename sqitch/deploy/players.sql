7aef32eb435fe2a94ec9b3d82c4fd020e6440026
Thomas Boyt <me@thomasboyt.com>
2016-09-05 15:10:34 -0400

    Make player name/auth token UNIQUE

--- sqitch/deploy/players.sql
+++ sqitch/deploy/players.sql
@@ -7,9 +7,9 @@ SET client_min_messages = 'warning';
 
 CREATE TABLE manygolf.players (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
+  name TEXT NOT NULL UNIQUE,
   color TEXT NOT NULL,
-  authentication_token TEXT NOT NULL,
+  authentication_token TEXT NOT NULL UNIQUE,
   timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
--- sqitch/revert/players.sql
+++ sqitch/revert/players.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
-DROP TABLE players;
+DROP TABLE manygolf.players;
 
 COMMIT;
